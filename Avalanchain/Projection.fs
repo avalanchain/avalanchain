8de35cf9e6162911b70a0a3db556c33135b43bfd
cotyar <khabarov@gmail.com>
2016-01-11 17:51:39 +0000

    ProjectionStore added

--- Avalanchain/Projection.fs
+++ Avalanchain/Projection.fs
@@ -7,6 +7,8 @@ open Chessie.ErrorHandling
 
 open SecKeys
 open RefsAndPathes
+open System.Collections.Generic
+open System.Linq
 
 //type HashableFunction<'T, 'TResult> = { 
 //    verifier: Verifier, func: Expr<Func<'T, 'TResult>>, proof: Proof) = 
@@ -20,27 +22,36 @@ type HashableFunction<'T, 'TResult> = {
     Expr: Expr<Func<'T, 'TResult>>
     Proof: Proof
 }
+with member inline this.Hash = this.Proof.ValueHash
 
 type HashableFunction<'T1, 'T2, 'TResult> = { 
     F : Func<'T1, 'T2, 'TResult>
     Expr: Expr<Func<'T1, 'T2, 'TResult>>
     Proof: Proof
 }
+with member inline this.Hash = this.Proof.ValueHash
 
 type SerializedFunction = Proof * Serialized
 
 type HashableFunctionBuilder<'T, 'TResult> = 
     ProofVerifier -> Deserializer<HashableFunction<'T, 'TResult>> -> SerializedFunction -> Result<HashableFunction<'T, 'TResult>, string> 
 
-let deserializeFunction (proofVerifier: ProofVerifier) deserializer (serializedFunction: Serialized) =
+type HashableFunctionBuilder<'T1, 'T2, 'TResult> = 
+    ProofVerifier -> Deserializer<HashableFunction<'T1, 'T2, 'TResult>> -> SerializedFunction -> Result<HashableFunction<'T1, 'T2, 'TResult>, string> 
+
+type FunctionSerializer<'T, 'TResult> = Signer -> Serializer<Expr<Func<'T, 'TResult>>> -> Hasher -> SerializedFunction
+
+type FunctionSerializer<'T1, 'T2, 'TResult> = Signer -> Serializer<Expr<Func<'T1, 'T2, 'TResult>>> -> Hasher -> SerializedFunction
+
+let deserializeFunction (proofVerifier: ProofVerifier) deserializer (serializedFunction: SerializedFunction) =
+    let checkProof sf =
+        if proofVerifier(fst sf) then ok sf else fail ("Proof verification failed") 
+
     let deserialize sf = 
         try
-            ok (deserializer(sf))
+            ok (fst sf, deserializer(snd sf))
         with
             | ex -> fail (sprintf "Error during deserialization : '%s'" (ex.ToString()))
-
-    let checkProof hf =
-        if proofVerifier(fst hf) then ok hf else fail ("Proof verification failed") 
 
     let compile hf =
         let proof, expr = hf
@@ -50,8 +61,8 @@ let deserializeFunction (proofVerifier: ProofVerifier) deserializer (serializedF
             | ex -> fail (sprintf "Error function compilation : '%s'" (ex.ToString()))
 
     let validate = 
-        deserialize 
-        >> bind checkProof
+        checkProof
+        >> bind deserialize 
         >> bind compile 
         >> bind (fun (proof, expr, f) -> 
                     ok {
@@ -62,9 +73,26 @@ let deserializeFunction (proofVerifier: ProofVerifier) deserializer (serializedF
 
     validate serializedFunction
 
-//type HashedFunction<'T> = Hashed<SerializableFunction<'T>>
+
+let serializeFunction signer serializer hasher expr =
+    let serialized = serializer expr
+    let hash = hasher serialized
+    let proof = {
+        Signature = signer hash
+        ValueHash = hash
+    }
+    proof, serialized
 
 // NOTE: Projection takes just event data and not the whole event
 //type Projection<'TState, 'TEventData> = 'TState -> 'TEventData -> ProjectionResult<'TState>
-type Projection<'TState, 'TEventData> = HashableFunction<'TState, 'TEventData, ProjectionResult<'TState>>
+type Projection<'TState, 'TData> = HashableFunction<'TState, 'TData, ProjectionResult<'TState>>
 and ProjectionResult<'TState> = Result<'TState, string>
+
+
+type ProjectionStorage<'TState, 'TData> (serializer, deserializer) = 
+    //Projection<'TState, 'TData> list
+    let projections = new Dictionary<Hash, Projection<'TState, 'TData>>()
+    member inline this.Add (projection: Projection<'TState, 'TData>) = projections.[projection.Hash] <- projection
+    member inline this.AddAll (projs: Projection<'TState, 'TData> seq) = projs |> Seq.iter (fun p -> projections.[p.Hash] <- p)
+    member inline this.Item hash = projections.TryGetValue(hash) |> (fun (b, res) -> if b then Some res else None)
+    
