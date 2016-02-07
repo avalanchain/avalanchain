5b50a2fc279743ccbe8971b34177874cb12ecdb1
cotyar <khabarov@gmail.com>
2016-02-07 02:29:03 +0000

    SqliteCluster persistence works

--- Avalanchain.Cluster/SqliteCluster.fs
+++ Avalanchain.Cluster/SqliteCluster.fs
@@ -82,38 +82,6 @@ type AutomaticCluster (system) =
         dbHelper.RemoveClusterMember(cluster.SelfAddress)
 
 
-type ShardedSystem (system) =
-    let automaticCluster = new AutomaticCluster(system)
-    let sharding = ClusterSharding.Get(system)
-    do automaticCluster.Join()
-    member this.System = system
-    member this.StartShardRegion<'Message, 'Returned> messageExtractor regionName (expr: Expr<Actor<'Message> -> Cont<'Message, 'Returned>>) (options : SpawnOption list)  = 
-        let e = Linq.Expression.ToExpression(fun () -> new FunActor<'Message, 'Returned>(expr))
-        let props = applySpawnOptions (Props.Create e) options
-        sharding.Start(regionName, props, ClusterShardingSettings.Create(system), messageExtractor)
-    member this.StartShardRegion2<'Message, 'Actor when 'Actor :> ActorBase> messageExtractor regionName props = 
-        sharding.Start(regionName, props, ClusterShardingSettings.Create(system), messageExtractor)
-    interface IDisposable with
-        member this.Dispose() = automaticCluster.Leave() // TODO: Implement the pattern properly
-
-
-
-let produceMessages (system: ActorSystem) (shardRegion: IActorRef) =
-    let entitiesCount = 20
-    let shardsCount = 10
-    let rand = new Random()
-
-    system.Scheduler.Advanced.ScheduleRepeatedly(
-        TimeSpan.FromSeconds(1.0), 
-        TimeSpan.FromSeconds(0.001), 
-        fun () ->
-            for i = 0 to 1 do
-                let shardId = rand.Next(shardsCount)
-                let entityId = rand.Next(entitiesCount)
-
-                shardRegion.Tell({ShardId = shardId.ToString(); EntityId = entityId.ToString(); Message = "hello world"})
-    )
-
 type ResActor<'TAdminCommand, 'TBusinessCommand, 'TState, 'TEvent>
     (
         updateState: 'TState -> 'TEvent -> 'TState, 
@@ -125,7 +93,7 @@ type ResActor<'TAdminCommand, 'TBusinessCommand, 'TState, 'TEvent>
     do (UntypedActor.Context.SetReceiveTimeout(Nullable(TimeSpan.FromMinutes(2.0))))
     member private __.Self = base.Self
     member private __.Context = UntypedActor.Context
-    override __.PersistenceId with get() = (sprintf "%s-%s" (self.Context.Parent.Path.Name) self.Self.Path.Name)
+    override __.PersistenceId with get() = (sprintf "Actor %s-%s" (self.Context.Parent.Path.Name) self.Self.Path.Name)
     override __.ReceiveRecover(msg: obj) = 
         match msg with 
         | :? 'TEvent as e -> 
@@ -151,7 +119,46 @@ type ResActor<'TAdminCommand, 'TBusinessCommand, 'TState, 'TEvent>
                         true
             | None -> false
         | _ -> false
-    
+
+type ShardedSystem (system) =
+    let automaticCluster = new AutomaticCluster(system)
+    let sharding = ClusterSharding.Get(system)
+    do automaticCluster.Join()
+    member this.System = system
+    member this.StartShardRegion<'Message, 'Returned> messageExtractor regionName (expr: Expr<Actor<'Message> -> Cont<'Message, 'Returned>>) (options : SpawnOption list)  = 
+        let e = Linq.Expression.ToExpression(fun () -> new FunActor<'Message, 'Returned>(expr))
+        let props = applySpawnOptions (Props.Create e) options
+        sharding.Start(regionName, props, ClusterShardingSettings.Create(system), messageExtractor)
+    member this.StartShardRegion2<'Message, 'Actor when 'Actor :> ActorBase> messageExtractor regionName props = 
+        sharding.Start(regionName, props, ClusterShardingSettings.Create(system), messageExtractor)
+    member this.StartShardRegion3<'Message, 'TAdminCommand, 'TBusinessCommand, 'TState, 'TEvent> messageExtractor regionName updateState processBusinessCommand processAdminCommand (options : SpawnOption list) = 
+        //let props = Props.Create<ResActor<'TAdminCommand, 'TBusinessCommand, 'TState, 'TEvent>>([|updateState, processBusinessCommand, processAdminCommand|])
+        let expr = <@ fun () -> new ResActor<'TAdminCommand, 'TBusinessCommand, 'TState, 'TEvent>(updateState, processBusinessCommand, processAdminCommand) @>
+        let props = Props.Create (Linq.Expression.ToExpression(expr))
+        let appliedProps = applySpawnOptions props options
+        sharding.Start(regionName, appliedProps, ClusterShardingSettings.Create(system), messageExtractor)
+    interface IDisposable with
+        member this.Dispose() = automaticCluster.Leave() // TODO: Implement the pattern properly
+
+
+
+let produceMessages (system: ActorSystem) (shardRegion: IActorRef) =
+    let entitiesCount = 20
+    let shardsCount = 10
+    let rand = new Random()
+
+    system.Scheduler.Advanced.ScheduleRepeatedly(
+        TimeSpan.FromSeconds(1.0), 
+        TimeSpan.FromSeconds(0.001), 
+        fun () ->
+            for i = 0 to 1 do
+                let shardId = rand.Next(shardsCount)
+                let entityId = rand.Next(entitiesCount)
+
+                shardRegion.Tell({ShardId = shardId.ToString(); EntityId = entityId.ToString(); Message = "hello world"})
+    )
+
+  
 
 
 let runExample (system: ActorSystem) =
@@ -171,27 +178,30 @@ let runExample (system: ActorSystem) =
     //let shardRegion = shardedSystem.StartShardRegion messageExtractor "printer" <@ actorOf (fun msg -> printfn "Shard Received: %s\n" msg) @> []
     //let shardRegion = shardedSystem.StartShardRegion2 messageExtractor "printer" (Props.Create<ResActor>())
 
-    // general update state method
-    let update state e = 
-        e::state
+    let shardRegion = shardedSystem.StartShardRegion3 messageExtractor "printer" 
+                        (fun state e -> e::state) (fun cmd -> Some(sprintf "Received '%s'" (cmd.ToString()))) (fun ac -> None) []
 
-    // apply is invoked when actor receives a recovery event
-    let apply _ = update
-
-    // exec is invoked when a actor receives a new message from another entity
-    let exec (mailbox: Eventsourced<_,ShardedMessage,_>) state cmd = 
-        printfn "Cmd Received: %A\n" cmd
-        mailbox.PersistEvent (update state) [cmd]
-//        match cmd with
-//        | "print" -> printf "State is: %A\n" state          // print current actor state
-//        | s       -> mailbox.PersistEvent (update state) [s]     // persist event and call update state on complete
-
-    let shardRegion = 
-        spawnPersist system "s0" {  // s0 identifies actor uniquelly across different incarnations
-            state = []              // initial state
-            apply = apply           // recovering function
-            exec = exec             // command handler
-        } []  
+//////    // general update state method
+//////    let update state e = 
+//////        e::state
+//////
+//////    // apply is invoked when actor receives a recovery event
+//////    let apply _ = update
+//////
+//////    // exec is invoked when a actor receives a new message from another entity
+//////    let exec (mailbox: Eventsourced<_,ShardedMessage,_>) state cmd = 
+//////        printfn "Cmd Received: %A\n" cmd
+//////        mailbox.PersistEvent (update state) [cmd]
+////////        match cmd with
+////////        | "print" -> printf "State is: %A\n" state          // print current actor state
+////////        | s       -> mailbox.PersistEvent (update state) [s]     // persist event and call update state on complete
+//////
+//////    let shardRegion = 
+//////        spawnPersist system "s0" {  // s0 identifies actor uniquelly across different incarnations
+//////            state = []              // initial state
+//////            apply = apply           // recovering function
+//////            exec = exec             // command handler
+//////        } []  
 
     System.Threading.Thread.Sleep(2000)
     //Console.Write("Press ENTER to start producing messages...")
