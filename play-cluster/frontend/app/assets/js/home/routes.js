35bd5b1f56d2cfc307fd8e5a5f627c726cc0019e
jackyrul <jackyrul@mail.ru>
2016-04-06 18:34:07 +0300

    minor fix

--- play-cluster/frontend/app/assets/js/home/routes.js
+++ play-cluster/frontend/app/assets/js/home/routes.js
@@ -7,7 +7,7 @@ define(['angular', './controllers', 'common'], function(angular, controllers) {
   var mod = angular.module('home.routes', ['yourprefix.common']);
   mod.config(['$routeProvider', function($routeProvider) {
     $routeProvider
-      .when('/',  {templateUrl: '/assets/partials/index.html',  controller:controllers.HomeCtrl});
+      .when('/t',  {templateUrl: '/assets/partials/dashboard/index.html',  controller:controllers.DashboardCtrl});
   }]);
   return mod;
 });
