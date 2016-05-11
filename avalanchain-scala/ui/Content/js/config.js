d26ef67f2befdf4a2fcdbe9175d75b0fc6dd1e0b
jackyrul <jackyrul@mail.ru>
2016-05-11 18:37:33 +0300

    add avalanchain web part

---
+++ avalanchain-scala/ui/Content/js/config.js
@@ -0,0 +1,171 @@
+/// <reference path="../views/clusters/clusters.html" />
+/**
+ * avalanchain - Responsive Admin Theme
+ * Copyright 2015 Webapplayers.com
+ *
+ * avalanchain theme use AngularUI Router to manage routing and views
+ * Each view are defined as state.
+ * Initial there are written state for all view in theme.
+ *
+ */
+function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, $locationProvider, adalAuthenticationServiceProvider, $httpProvider) {
+    //$locationProvider.html5Mode({
+    //    enabled: true,
+    //    requireBase: false
+    //});
+    //$locationProvider.html5Mode(true).hashPrefix('!');
+    $urlRouterProvider.otherwise("/index/streams");
+
+    $ocLazyLoadProvider.config({
+        // Set to true if you want to see what and when is dynamically loaded
+        debug: false
+    });
+
+    var endpoints = {
+        "http://localhost:48213": "https://avlnchdemo.azurewebsites.net"
+        // Map the location of a request to an API to a the identifier of the associated resource
+        //"Enter the root location of your To Go API here, e.g. https://contosotogo.azurewebsites.net/":
+        //    "Enter the App ID URI of your To Go API here, e.g. https://contoso.onmicrosoft.com/ToGoAPI",
+    };
+
+    //adalAuthenticationServiceProvider.init(
+    //    {
+    //        instance: 'https://login.microsoftonline.com/',
+    //        tenant: 'safytrack.onmicrosoft.com',
+    //        clientId: '02fa79b9-d917-4496-93d4-8fb311611f51',
+    //        extraQueryParameter: 'nux=1',
+    //        //endpoints: endpoints,
+    //        //cacheLocation: 'localStorage', // enable this for IE, as sessionStorage does not work for localhost.
+    //    },
+    //    $httpProvider
+    //    );
+
+    $stateProvider
+
+        .state('dashboards', {
+            abstract: true,
+            url: "/dashboards",
+            templateUrl: "../Content/views/common/content.html",
+        })
+        .state('quoka', {
+            abstract: true,
+            url: "/quoka",
+            templateUrl: "../Content/views/common/content.html",
+        })
+        .state('index', {
+            abstract: true,
+            url: "/index",
+            templateUrl: "../Content/views/common/content.html",
+        })
+        .state('dashboards.dashboard', {
+            url: "/dashboard",
+            templateUrl: "../Content/views/dashboard/dashboard.html",
+            data: { pageTitle: 'dashboard' },
+
+            //requireADLogin: true,
+            resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+                            serie: true,
+                            name: 'angular-flot',
+                            files: ['../Content/js/plugins/flot/jquery.flot.js', '../Content/js/plugins/flot/jquery.flot.time.js', '../Content/js/plugins/flot/jquery.flot.tooltip.min.js', '../Content/js/plugins/flot/jquery.flot.spline.js', '../Content/js/plugins/flot/jquery.flot.resize.js', '../Content/js/plugins/flot/jquery.flot.pie.js', '../Content/js/plugins/flot/curvedLines.js', '../Content/js/plugins/flot/angular-flot.js', ]
+                        }
+                    ]);
+                }
+            }
+        })
+        .state('quoka.dashboard', {
+            url: "/dashboard",
+            templateUrl: "../Content/views/quoka/dashboard.html",
+            data: { pageTitle: 'Dashboard' },
+            resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+                            serie: true,
+                            name: 'angular-flot',
+                            files: ['../Content/js/plugins/flot/jquery.flot.js', '../Content/js/plugins/flot/jquery.flot.time.js', '../Content/js/plugins/flot/jquery.flot.tooltip.min.js', '../Content/js/plugins/flot/jquery.flot.spline.js', '../Content/js/plugins/flot/jquery.flot.resize.js', '../Content/js/plugins/flot/jquery.flot.pie.js', '../Content/js/plugins/flot/curvedLines.js', '../Content/js/plugins/flot/angular-flot.js', ]
+                        },
+                        {
+                            serie: true,
+                            files: ['../Content/js/plugins/jvectormap/jquery-jvectormap-2.0.2.min.js', '../Content/js/plugins/jvectormap/jquery-jvectormap-2.0.2.css']
+                        },
+                        {
+                            serie: true,
+                            files: ['../Content/js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js']
+                        },
+                        {
+                            name: 'ui.checkbox',
+                            files: ['../Content/js/bootstrap/angular-bootstrap-checkbox.js']
+                        },
+                        {
+                            serie: true,
+                            files: ['../Content/css/plugins/c3/c3.min.css', '../Content/js/plugins/d3/d3.min.js', '../Content/js/plugins/c3/c3.min.js']
+                        },
+                        {
+                            serie: true,
+                            name: 'gridshore.c3js.chart',
+                            files: ['../Content/js/plugins/c3/c3-angular.min.js']
+                        }
+                    ]);
+                }
+            }
+        })
+        .state('quoka.trader', {
+            url: "/trader",
+            templateUrl: "../Content/views/quoka/trader.html",
+            data: { pageTitle: 'quoka trader' }
+        })
+        .state('index.clusters', {
+            url: "/clusters",
+            templateUrl: "../Content/views/clusters/clusters.html",
+            data: { pageTitle: 'clusters' }
+        })
+        // .state('index.accounts', {
+        //     url: "/accounts",
+        //     templateUrl: "/Content/views/accounts/accounts.html",
+        //     data: { pageTitle: 'accounts' }
+        // })
+        .state('index.trader', {
+            url: "/trader",
+            templateUrl: "../Content/views/trader/trader.html",
+            data: { pageTitle: 'trader' }
+        })
+        .state('index.nodes', {
+            url: "/nodes",
+            templateUrl: "../Content/views/nodes/nodes.html",
+            data: { pageTitle: 'nodes' }
+        })
+        .state('index.streams', {
+            url: "/streams",
+            templateUrl: "../Content/views/streams/streams.html",
+            data: { pageTitle: 'streams' }
+        })
+        .state('index.doctor', {
+            url: "/doctor",
+            templateUrl: "../Content/views/doctor/doctor.html",
+            data: { pageTitle: 'doctor' }
+        })
+    .state('index.admin', {
+        url: "/admin",
+        templateUrl: "../Content/views/admin/admin.html",
+        data: { pageTitle: 'admin' }
+    });
+        //.state('index.main', {
+        //    url: "/main",
+        //    templateUrl: "/Content/views/main.html",
+        //    data: { pageTitle: 'Example view' }
+        //})
+        //.state('index.minor', {
+        //    url: "/minor",
+        //    templateUrl: "/Content/views/minor.html",
+        //    data: { pageTitle: 'Example view' }
+        //})
+}
+angular
+    .module('avalanchain')
+    .config(config)
+    .run(function ($rootScope, $state) {
+        $rootScope.$state = $state;
+    });
---
+++ avalanchain-scala/ui/Content/views/trader/trader.js
@@ -0,0 +1,135 @@
+(function () {
+    'use strict';
+    var controllerId = 'Trader';
+    angular.module('avalanchain').controller(controllerId, ['common', '$scope', 'dataservice', trader]);
+
+    function trader(common, $scope, dataservice) {
+        var getLogFn = common.logger.getLogFn;
+        var log = getLogFn(controllerId);
+
+        this.info = 'quoka trader';
+        this.helloText = 'Welcome in Avalanchain';
+        this.descriptionText = 'CASCADING REACTIVE BLOCKCHAINS';
+        $scope.datayahoo = [];
+        $scope.users = [1, 2, 3, 4];
+        $scope.amount1 = [1000, 2100, 3330, 400];
+        $scope.amount2 = [1100, 2200, 133000, 1400];
+        $scope.transactions = [];
+        $scope.transactionPage = 1;
+        $scope.isEdit = false;
+
+        setInterval(function updateRandom() {
+            if (!$scope.isEdit)
+                getData();
+        }, 3000);
+
+
+        var masteruser = {};
+        $scope.edit = function (user) {
+            masteruser = angular.copy(user);
+            $scope.changeview(user);
+        }
+        $scope.save = function (user) {
+            $scope.changeview(user);
+        }
+        $scope.cancel = function (user) {
+            user.max = masteruser.max;
+            user.min = masteruser.min;
+            $scope.changeview(user);
+        }
+        $scope.changeview = function (user) {
+            user.isEdit = !user.isEdit;
+            $scope.isEdit = !$scope.isEdit;
+        }
+
+        var dataprev = [];
+
+        function addAmount(data) {
+            var isFirst = $scope.users[0]["max"] >= 0 ? false : true;
+            if (data) {
+
+                for (var i = 0; i < $scope.amount2.length; i++) {
+                    if (dataprev.length === 0) dataprev = data;
+
+                    if (dataprev[i].Rate > data[i].Rate) {
+                        data[i]["status"] = 'text-danger';
+                        data[i]["navigation"] = 'fa fa-play fa-rotate-90';
+                    }
+                    else if (dataprev[i].Rate < data[i].Rate) {
+                        data[i]["status"] = 'text-navy';
+                        data[i]["navigation"] = 'fa fa-play fa-rotate-270';
+                    } else {
+                        data[i]["status"] = '';
+                        data[i]["navigation"] = 'fa fa-pause';
+                    }
+                    data[i]["amount1"] = $scope.amount1[i];
+                    data[i]["amount2"] = $scope.amount2[i];
+                    if (isFirst) {
+                        var ask = parseFloat(data[i].Ask);
+                        var bid = parseFloat(data[i].Bid);
+                        data[i]["max"] = ask + (ask - bid);
+                        data[i]["min"] = ask - (ask - bid);
+                    } else {
+                        data[i]["max"] = $scope.users[i].max;
+                        data[i]["min"] = $scope.users[i].min;;
+                    }
+                    data[i]["from"] = data[i].Name.split('/')[0];
+                    data[i]["to"] = data[i].Name.split('/')[1];
+                    data[i]["isEdit"] = false;
+                    data[i] = action(data[i]);
+                    $scope.amount1[i] = data[i].amount1;
+                    $scope.amount2[i] = data[i].amount2;
+                }
+            }
+            dataprev = data;
+            return data;
+        }
+        function action(user) {
+            var sell = 40;
+            var buy = 40;
+            if (user.max !== 0 && user.max > user.Ask && user.amount1 >= sell) {
+                user.amount1 = user.amount1 - sell;
+                user.amount2 = user.amount2 + sell * user.Ask;
+
+                $scope.transactions.unshift({
+                    from: 'Sell ' + user.Name.split('/')[0],
+                    amountFrom: sell,
+                    name: user.Name,
+                    to: 'Buy ' + user.Name.split('/')[1],
+                    amountTo: sell * user.Ask,
+                    time: user.Time
+                });
+                if ($scope.transactions.length > 100) $scope.transactions.pop();
+            }
+
+            if (user.min !== 0 && user.min <= user.Bid && user.amount2 >= buy * user.Bid) {
+                user.amount2 = user.amount2 - buy * user.Bid;
+                user.amount1 = user.amount1 + buy;
+
+                $scope.transactions.unshift({
+                    from: 'Sell ' + user.Name.split('/')[1],
+                    amountFrom: buy * user.Bid,
+                    name: user.Name,
+                    to: 'Buy ' + user.Name.split('/')[0],
+                    amountTo: buy,
+                    time: user.Time
+                });
+                if ($scope.transactions.length > 100) $scope.transactions.pop();
+            }
+            return user;
+        }
+
+        function getData() {
+            return dataservice.getYData().then(function (data) {
+                $scope.users = addAmount(data.data.query.results.rate);
+            });
+        }
+        activate();
+        function activate() {
+            common.activateController([getData()], controllerId)
+                .then(function () { log('Activated trader') });//log('Activated Admin View');
+        }
+    };
+
+
+})();
