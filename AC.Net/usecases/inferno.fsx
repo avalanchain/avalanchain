b6ef809330134927a7370d00937c8cca8c74abe7
cotyar <khabarov@gmail.com>
2017-06-27 12:48:18 +0100

    Splitting Inferno and BC playgrounds

---
+++ AC.Net/usecases/inferno.fsx
@@ -0,0 +1,27 @@
+#r "../packages/Inferno/lib/net452/SecurityDriven.Inferno.dll"
+
+open System
+open System.Security.Cryptography
+open SecurityDriven.Inferno
+open SecurityDriven.Inferno.Extensions
+
+#time
+
+let dsaKeyPrivate = CngKeyExtensions.CreateNewDsaKey()
+let dsaKeyPrivateBlob = dsaKeyPrivate.GetPrivateBlob()
+let dsaKeyPublicBlob = dsaKeyPrivate.GetPublicBlob()
+let dsaKeyPublic: CngKey = dsaKeyPublicBlob.ToPublicKeyFromBlob()
+
+use ecdsa = new ECDsaCng(dsaKeyPrivate) 
+ecdsa.HashAlgorithm <- CngAlgorithm.Sha384 // generate DSA signature with private key
+
+let rnd = Random();
+
+let upperBound = 1000
+let data = [| for i in 0 .. upperBound - 1 -> 
+                let d = Array.create<byte> 100 0uy
+                rnd.NextBytes(d)
+                d |]
+
+let sigs = [| for i in 0 .. upperBound - 1 -> ecdsa.SignData(data.[i]) |]
+let verfs = [| for i in 0 .. upperBound - 1 -> ecdsa.VerifyData(data.[i], sigs.[i]) |]
--- AC.Net/usecases/x509.fsx
+++ AC.Net/usecases/x509.fsx
@@ -1,5 +1,4 @@
 #r "../packages/BouncyCastle/lib/BouncyCastle.Crypto.dll"
-#r "../packages/Inferno/lib/net452/SecurityDriven.Inferno.dll"
 
 open System
 open Org.BouncyCastle.Asn1.X509
@@ -12,36 +11,7 @@ open Org.BouncyCastle.Crypto.Generators
 open Org.BouncyCastle.Pkcs
 open Org.BouncyCastle.X509
 
-open System.Security.Cryptography
-open SecurityDriven.Inferno
-open SecurityDriven.Inferno.Extensions
-
 #time
-
-let dsaKeyPrivate = CngKeyExtensions.CreateNewDsaKey()
-let dsaKeyPrivateBlob = dsaKeyPrivate.GetPrivateBlob()
-let dsaKeyPublicBlob = dsaKeyPrivate.GetPublicBlob()
-let dsaKeyPublic: CngKey = dsaKeyPublicBlob.ToPublicKeyFromBlob()
-
-use ecdsa = new ECDsaCng(dsaKeyPrivate) 
-ecdsa.HashAlgorithm <- CngAlgorithm.Sha384 // generate DSA signature with private key
-
-let rnd = Random();
-
-let upperBound = 1000
-let data = [| for i in 0 .. upperBound - 1 -> 
-                let d = Array.create<byte> 100 0uy
-                rnd.NextBytes(d)
-                d |]
-
-let sigs = [| for i in 0 .. upperBound - 1 -> ecdsa.SignData(data.[i]) |]
-let verfs = [| for i in 0 .. upperBound - 1 -> ecdsa.VerifyData(data.[i], sigs.[i]) |]
-
-
-
-// let kpg = RsaKeyPairGenerator()
-// kpg.Init(KeyGenerationParameters(SecureRandom(CryptoApiRandomGenerator()), 1024))
-// let kp = kpg.GenerateKeyPair()
 
 let gen = X509V3CertificateGenerator()
 let certName = X509Name("CN=PickAName")
