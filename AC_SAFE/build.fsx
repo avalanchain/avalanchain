2a80bb65c2ab993ff968857976f71b464e534365
cotyar <khabarov@gmail.com>
2018-03-08 17:25:19 +0000

    revert build.fsx

--- AC_SAFE/build.fsx
+++ AC_SAFE/build.fsx
@@ -1,305 +1,117 @@
-// --------------------------------------------------------------------------------------
-// FAKE build script
-// --------------------------------------------------------------------------------------
-
 #r @"packages/build/FAKE/tools/FakeLib.dll"
 
-open Fake
-open Fake.Git
-open Fake.AssemblyInfoFile
-open Fake.ReleaseNotesHelper
 open System
-open System.IO
-open Fake.Testing.Expecto
 
-let project = "Suave/Fable sample"
+open Fake
 
-let summary = "SAFE sample"
-
-let description = summary
-
-let configuration = "Release"
-
+let serverPath = "./src/Server" |> FullName
 let clientPath = "./src/Client" |> FullName
-
-let serverPath = "./src/Server/" |> FullName
-
-let serverTestsPath = "./test/ServerTests" |> FullName
-let clientTestsPath = "./test/UITests" |> FullName
-
-let dotnetcliVersion = DotNetCli.GetDotNetSDKVersionFromGlobalJson()
-let mutable dotnetExePath = "dotnet"
-
-let deployDir = "./deploy"
-
-// Pattern specifying assemblies to be tested using expecto
-let clientTestExecutables = "test/UITests/**/bin/**/*Tests*.exe"
-
-let dockerUser = getBuildParam "DockerUser"
-let dockerPassword = getBuildParam "DockerPassword"
-let dockerLoginServer = getBuildParam "DockerLoginServer"
-let dockerImageName = getBuildParam "DockerImageName"
-
-// --------------------------------------------------------------------------------------
-// END TODO: The rest of the file includes standard build steps
-// --------------------------------------------------------------------------------------
-
-let run' timeout cmd args dir =
-    if execProcess (fun info ->
-        info.FileName <- cmd
-        if not (String.IsNullOrWhiteSpace dir) then
-            info.WorkingDirectory <- dir
-        info.Arguments <- args
-    ) timeout |> not then
-        failwithf "Error while running '%s' with args: %s" cmd args
-
-let run = run' System.TimeSpan.MaxValue
-
-let runDotnet workingDir args =
-    let result =
-        ExecProcess (fun info ->
-            info.FileName <- dotnetExePath
-            info.WorkingDirectory <- workingDir
-            info.Arguments <- args) TimeSpan.MaxValue
-    if result <> 0 then failwithf "dotnet %s failed" args
+let deployDir = "./deploy" |> FullName
 
 let platformTool tool winTool =
-    let tool = if isUnix then tool else winTool
-    tool
-    |> ProcessHelper.tryFindFileOnPath
-    |> function Some t -> t | _ -> failwithf "%s not found" tool
+  let tool = if isUnix then tool else winTool
+  tool
+  |> ProcessHelper.tryFindFileOnPath
+  |> function Some t -> t | _ -> failwithf "%s not found" tool
 
 let nodeTool = platformTool "node" "node.exe"
-let npmTool = platformTool "npm" "npm.cmd"
-let yarnTool = platformTool "yarn" "yarn.cmd"
+let npmTool = platformTool "npm" "npm.cmd"  
 
-do if not isWindows then
-    // We have to set the FrameworkPathOverride so that dotnet sdk invocations know
-    // where to look for full-framework base class libraries
-    let mono = platformTool "mono" "mono"
-    let frameworkPath = IO.Path.GetDirectoryName(mono) </> ".." </> "lib" </> "mono" </> "4.5"
-    setEnvironVar "FrameworkPathOverride" frameworkPath
+let dotnetcliVersion = DotNetCli.GetDotNetSDKVersionFromGlobalJson()
+let mutable dotnetCli = "dotnet"
 
+let run cmd args workingDir =
+  let result =
+    ExecProcess (fun info ->
+      info.FileName <- cmd
+      info.WorkingDirectory <- workingDir
+      info.Arguments <- args) TimeSpan.MaxValue
+  if result <> 0 then failwithf "'%s %s' failed" cmd args
 
-// Read additional information from the release notes document
-let releaseNotes = File.ReadAllLines "RELEASE_NOTES.md"
-
-let releaseNotesData =
-    releaseNotes
-    |> parseAllReleaseNotes
-
-let release = List.head releaseNotesData
-
-let packageVersion = SemVerHelper.parse release.NugetVersion
-
-
-// --------------------------------------------------------------------------------------
-// Clean build results
-
-Target "Clean" (fun _ ->
-    !!"src/**/bin"
-    ++ "test/**/bin"
-    |> CleanDirs
-
-    !! "src/**/obj/*.nuspec"
-    ++ "test/**/obj/*.nuspec"
-    |> DeleteFiles
-
-    CleanDirs ["bin"; "temp"; "docs/output"; deployDir; Path.Combine(clientPath,"public/bundle")]
+Target "Clean" (fun _ -> 
+  CleanDirs [deployDir]
 )
 
 Target "InstallDotNetCore" (fun _ ->
-    dotnetExePath <- DotNetCli.InstallDotNetSDK dotnetcliVersion
-)
-
-// --------------------------------------------------------------------------------------
-// Build library & test project
-
-
-Target "BuildServer" (fun _ ->
-    runDotnet serverPath "build"
+  dotnetCli <- DotNetCli.InstallDotNetSDK dotnetcliVersion
 )
 
 Target "InstallClient" (fun _ ->
-    printfn "Node version:"
-    run nodeTool "--version" __SOURCE_DIRECTORY__
-    printfn "Yarn version:"
-    run yarnTool "--version" __SOURCE_DIRECTORY__
-    run yarnTool "install --frozen-lockfile" __SOURCE_DIRECTORY__
+  printfn "Node version:"
+  run nodeTool "--version" __SOURCE_DIRECTORY__
+  printfn "Npm version:"
+  run npmTool "--version"  __SOURCE_DIRECTORY__
+  run npmTool "install" __SOURCE_DIRECTORY__
+  run dotnetCli "restore" clientPath
 )
 
-Target "BuildClient" (fun _ ->
-    runDotnet clientPath "restore"
-    runDotnet clientPath "fable webpack --port free -- -p"
+Target "RestoreServer" (fun () -> 
+  run dotnetCli "restore" serverPath
 )
 
-
-// --------------------------------------------------------------------------------------
-// Run the Website
-
-let ipAddress = "localhost"
-let port = 8080
-
-FinalTarget "KillProcess" (fun _ ->
-    killProcess "dotnet"
-    killProcess "dotnet.exe"
+Target "Build" (fun () ->
+  run dotnetCli "build" serverPath
+  run dotnetCli "fable webpack -- -p" clientPath
 )
 
+Target "Run" (fun () ->
+  let server = async {
+    printfn "%s" serverPath
+    run dotnetCli "watch run" serverPath
+  }
+  let client = async {
+    run dotnetCli "fable webpack-dev-server" clientPath
+  }
+  let browser = async {
+    Threading.Thread.Sleep 5000
+    Diagnostics.Process.Start "http://localhost:8080" |> ignore
+  }
 
-Target "Run" (fun _ ->
-    runDotnet clientPath "restore"
-    runDotnet serverTestsPath "restore"
-
-    let unitTestsWatch = async {
-        let result =
-            ExecProcess (fun info ->
-                info.FileName <- dotnetExePath
-                info.WorkingDirectory <- serverTestsPath
-                info.Arguments <- sprintf "watch msbuild /t:TestAndRun /p:DotNetHost=%s" dotnetExePath) TimeSpan.MaxValue
-
-        if result <> 0 then failwith "Website shut down." }
-
-    let fablewatch = async { runDotnet clientPath "fable webpack-dev-server" }
-    let openBrowser = async {
-        System.Threading.Thread.Sleep(5000)
-        Diagnostics.Process.Start("http://"+ ipAddress + sprintf ":%d" port) |> ignore }
-
-    Async.Parallel [| unitTestsWatch; fablewatch; openBrowser |]
-    |> Async.RunSynchronously
-    |> ignore
+  //[ server; client; browser]
+  [ server]
+  |> Async.Parallel
+  |> Async.RunSynchronously
+  |> ignore
 )
 
+Target "Bundle" (fun _ ->
+  let serverDir = deployDir </> "Server"
+  let clientDir = deployDir </> "Client"
+  
+  let publicDir = clientDir </> "public"
 
-// --------------------------------------------------------------------------------------
-// Release Scripts
+  let publishArgs = sprintf "publish -c Release -o \"%s\"" serverDir
+  run dotnetCli publishArgs serverPath
 
+  !! "src/Client/public/**/*.*" |> CopyFiles publicDir
 
-Target "SetReleaseNotes" (fun _ ->
-    let lines = [
-            "module internal ReleaseNotes"
-            ""
-            (sprintf "let Version = \"%s\"" release.NugetVersion)
-            ""
-            (sprintf "let IsPrerelease = %b" (release.SemVer.PreRelease <> None))
-            ""
-            "let Notes = \"\"\""] @ Array.toList releaseNotes @ ["\"\"\""]
-    File.WriteAllLines("src/Client/ReleaseNotes.fs",lines)
+  !! "src/Client/index.html"
+  ++ "src/Client/*.css"
+  |> CopyFiles clientDir 
 )
 
-Target "PrepareRelease" (fun _ ->
-    Git.Branches.checkout "" false "master"
-    Git.CommandHelper.directRunGitCommand "" "fetch origin" |> ignore
-    Git.CommandHelper.directRunGitCommand "" "fetch origin --tags" |> ignore
+let dockerUser = "safe-template"
+let dockerImageName = "safe-template"
 
-    StageAll ""
-    Git.Commit.Commit "" (sprintf "Bumping version to %O" release.NugetVersion)
-    Git.Branches.pushBranch "" "origin" "master"
+let dockerFullName = sprintf "%s/%s" dockerUser dockerImageName
 
-    let tagName = string release.NugetVersion
-    Git.Branches.tag "" tagName
-    Git.Branches.pushTag "" "origin" tagName
+Target "Docker" (fun _ ->
+  let buildArgs = sprintf "build -t %s ." dockerFullName
+  run "docker" buildArgs "."
 
-    let result =
-        ExecProcess (fun info ->
-            info.FileName <- "docker"
-            info.Arguments <- sprintf "tag %s/%s %s/%s:%s" dockerUser dockerImageName dockerUser dockerImageName release.NugetVersion) TimeSpan.MaxValue
-    if result <> 0 then failwith "Docker tag failed"
+  let tagArgs = sprintf "tag %s %s" dockerFullName dockerFullName
+  run "docker" tagArgs "."
 )
-
-Target "BundleClient" (fun _ ->
-    let result =
-        ExecProcess (fun info ->
-            info.FileName <- dotnetExePath
-            info.WorkingDirectory <- serverPath
-            info.Arguments <- "publish -c Release -o \"" + FullName deployDir + "\"") TimeSpan.MaxValue
-    if result <> 0 then failwith "Publish failed"
-
-    let clientDir = deployDir </> "client"
-    let publicDir = clientDir </> "public"
-    let jsDir = clientDir </> "js"
-    let cssDir = clientDir </> "css"
-    let imageDir = clientDir </> "Images"
-
-    !! "src/Client/public/**/*.*" |> CopyFiles publicDir
-    !! "src/Client/js/**/*.*" |> CopyFiles jsDir
-    !! "src/Client/css/**/*.*" |> CopyFiles cssDir
-    !! "src/Client/Images/**/*.*" |> CopyFiles imageDir
-
-    "src/Client/index.html" |> CopyFile clientDir
-)
-
-Target "CreateDockerImage" (fun _ ->
-    if String.IsNullOrEmpty dockerUser then
-        failwithf "docker username not given."
-    if String.IsNullOrEmpty dockerImageName then
-        failwithf "docker image Name not given."
-    let result =
-        ExecProcess (fun info ->
-            info.FileName <- "docker"
-            info.UseShellExecute <- false
-            info.Arguments <- sprintf "build -t %s/%s ." dockerUser dockerImageName) TimeSpan.MaxValue
-    if result <> 0 then failwith "Docker build failed"
-)
-
-Target "TestDockerImage" (fun _ ->
-    ActivateFinalTarget "KillProcess"
-    let testImageName = "test"
-
-    let result =
-        ExecProcess (fun info ->
-            info.FileName <- "docker"
-            info.Arguments <- sprintf "run -d -p 127.0.0.1:8085:8085 --rm --name %s -it %s/%s" testImageName dockerUser dockerImageName) TimeSpan.MaxValue
-    if result <> 0 then failwith "Docker run failed"
-
-    System.Threading.Thread.Sleep 5000 |> ignore  // give server some time to start
-
-    !! clientTestExecutables
-    |> Expecto (fun p -> { p with Parallel = false } )
-    |> ignore
-
-    let result =
-        ExecProcess (fun info ->
-            info.FileName <- "docker"
-            info.Arguments <- sprintf "stop %s" testImageName) TimeSpan.MaxValue
-    if result <> 0 then failwith "Docker stop failed"
-)
-
-Target "Deploy" (fun _ ->
-    let result =
-        ExecProcess (fun info ->
-            info.FileName <- "docker"
-            info.WorkingDirectory <- deployDir
-            info.Arguments <- sprintf "login %s --username \"%s\" --password \"%s\"" dockerLoginServer dockerUser dockerPassword) TimeSpan.MaxValue
-    if result <> 0 then failwith "Docker login failed"
-
-    let result =
-        ExecProcess (fun info ->
-            info.FileName <- "docker"
-            info.WorkingDirectory <- deployDir
-            info.Arguments <- sprintf "push %s/%s" dockerUser dockerImageName) TimeSpan.MaxValue
-    if result <> 0 then failwith "Docker push failed"
-)
-
-// -------------------------------------------------------------------------------------
-Target "Build" DoNothing
-Target "All" DoNothing
 
 "Clean"
   ==> "InstallDotNetCore"
   ==> "InstallClient"
-  ==> "BuildServer"
-  ==> "BuildClient"
-  ==> "BundleClient"
-  ==> "All"
-  ==> "CreateDockerImage"
-  ==> "TestDockerImage"
-  ==> "PrepareRelease"
-  ==> "Deploy"
-
-"BuildClient"
   ==> "Build"
+  ==> "Bundle"
+  ==> "Docker"
 
 "InstallClient"
+  ==> "RestoreServer"
   ==> "Run"
 
-RunTargetOrDefault "All"
+RunTargetOrDefault "Build"
