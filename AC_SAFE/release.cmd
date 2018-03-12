2caefaa38d74d3c604d7cd15607ade1d49732f6c
jackyrul <jackyrul92@gmail.com>
2018-03-12 18:00:00 +0200

    removed release.cmd

--- AC_SAFE/release.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-cls
-
-.paket\paket.exe restore
-if errorlevel 1 (
-  exit /b %errorlevel%
-)
-
-packages\build\FAKE\tools\FAKE.exe build.fsx Deploy "DockerLoginServer=docker.io" "DockerImageName=****" "DockerUser=jackyrul" "DockerPassword=7924655q" %*
